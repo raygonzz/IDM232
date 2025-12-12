8823a2b2a1479005ec71fb9eedba16d854ffcf35
raygonzz <127235411+raygonzz@users.noreply.github.com>
2025-12-11 19:59:00 -0500

    change directory for alpha

--- final/rg938_db.sql
+++ final/rg938_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 11, 2025 at 09:46 PM
+-- Generation Time: Dec 11, 2025 at 11:58 PM
 -- Server version: 8.0.40
 -- PHP Version: 8.3.14
 
@@ -18,8 +18,47 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `IDM232`
+-- Database: `restapi`
 --
+CREATE DATABASE IF NOT EXISTS `restapi` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `restapi`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restapi`
+--
+
+CREATE TABLE `restapi` (
+  `ID` int NOT NULL,
+  `Name` varchar(60) NOT NULL,
+  `Email` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `restapi`
+--
+ALTER TABLE `restapi`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `restapi`
+--
+ALTER TABLE `restapi`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
+--
+-- Database: `rg938_db`
+--
+CREATE DATABASE IF NOT EXISTS `rg938_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `rg938_db`;
 
 -- --------------------------------------------------------
 
